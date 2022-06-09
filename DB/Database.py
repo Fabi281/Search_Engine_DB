59d9c34ce1a35807657614cfb6f001bc7b22047a
Fabi281 <Fabian.Meindl@freenet.de>
2022-06-09 17:48:56 +0200

    Unique Link und Word + Return ID

--- DB/Database.py
+++ DB/Database.py
@@ -31,8 +31,8 @@ class Database:
             )
             self.cur = self.conn.cursor()
             
-            self.cur.execute("CREATE TABLE IF NOT EXISTS link (id INT NOT NULL AUTO_INCREMENT, url VARCHAR(255) NOT NULL, PRIMARY KEY (id))")
-            self.cur.execute("CREATE TABLE IF NOT EXISTS word (id INT NOT NULL AUTO_INCREMENT, word VARCHAR(255) NOT NULL, PRIMARY KEY (id))")
+            self.cur.execute("CREATE TABLE IF NOT EXISTS link (id INT NOT NULL AUTO_INCREMENT, url VARCHAR(255), language VARCHAR(255)  NOT NULL, PRIMARY KEY (id), UNIQUE (url))")
+            self.cur.execute("CREATE TABLE IF NOT EXISTS word (id INT NOT NULL AUTO_INCREMENT, word VARCHAR(255) NOT NULL, PRIMARY KEY (id), UNIQUE (word))")
             self.cur.execute("CREATE TABLE IF NOT EXISTS wordrelation (word_id INT NOT NULL, link_id INT NOT NULL, weight INT, PRIMARY KEY (word_id, link_id), FOREIGN KEY (word_id) REFERENCES word(id), FOREIGN KEY (link_id) REFERENCES link(id))")
         except mariadb.Error as e:
             print(f"Error connecting to MariaDB Platform: {e}")
@@ -48,40 +48,56 @@ class Database:
             print(f"Error executing query: {e}")
         return self.cur.fetchall()
 
-    def insert_single_into_single_table(self, table, values):
+    def insert_single_into_single_table(self, table, value):
 
         '''Please provide a string of tablename + columnname the value should be added at. Values can also be a list of values with 
         the same pattern.
-        Example: insert_into_single_table(Database.Table.word.value, "'test'") <word>
-        Example: insert_into_single_table(Database.Table.link.value, "'www.heidenheim.dhbw.de/'") <url>
-        Example: insert_into_single_table(Database.Table.wordrelation.value, "1, 1, 1") <word_id>, <link_id>, <weight>
+        Example: insert_single_into_single_table(Database.Table.word.value, ("test",)) => <word>
+        Example: insert_single_into_single_table(Database.Table.link.value, ("www.google.com", "German")) => <url>, <language>
+        Example: insert_single_into_single_table(Database.Table.wordrelation.value, (1,1,1)) => <word_id>, <link_id>, <weight>
         '''
 
+        id = None
+        value = ["'%s'" % w for w in value]
+
+        # Ich bin aktuell noch auf Python 3.9 deswegen kein Match
         if(table == self.Table.word.value):
-            mySql_insert_query = f"INSERT INTO word (word) VALUES ({values})"
+            mySql_insert_query = "INSERT INTO word (word) VALUES (%s)" % value[0]
         elif(table == self.Table.link.value):
-            mySql_insert_query = f"INSERT INTO link (url) VALUES ({values})"
+            mySql_insert_query = """INSERT INTO link (url, language) VALUES (%s, %s)""" % (value[0], value[1])
         elif(table == self.Table.wordrelation.value):
-            mySql_insert_query = f"INSERT INTO wordrelation (word_id, link_id, weight) VALUES ({values})"
+            mySql_insert_query = """INSERT INTO wordrelation (word_id, link_id, weight) VALUES (%s, %s, %s)""" % (value[0], value[1], value[2])
+
         try:
-            self.cur.execute(mySql_insert_query, values)
+            self.cur.execute(mySql_insert_query)
             self.conn.commit()
         except mariadb.Error as e:
                 print(f"Error: {e}")
+
+        if(table == self.Table.word.value):
+            id = self.get_from_query("SELECT id FROM word WHERE word = %s" % value[0])
+        elif(table == self.Table.link.value):
+            id = self.get_from_query(f"SELECT id FROM link WHERE link.url = {value[0]} AND link.language = {value[1]}")
+        elif(table == self.Table.wordrelation.value):
+            id = self.get_from_query(f"SELECT word_id, link_id FROM wordrelation WHERE wordrelation.word_id = {value[0]} AND wordrelation.link_id = {value[1]} AND wordrelation.weight = {value[2]}")
+
+        return id
     
     def insert_multiple_into_single_table(self, table, values):
 
         '''Please provide a string of tablename + columnname the value should be added at. Values can also be a list of values with 
         the same pattern.
-        Example: insert_multiple_into_single_table(Database.Table.word.value, [("wordTuple1",), ("<word>",)])  - IMPORTANT the comma afterwards is needed
-                 insert_multiple_into_single_table(Database.Table.link.value, [("'https://stackoverflow.com/'",), ("'<url>'",)])
-                 insert_multiple_into_single_table(Database.Table.wordrelation.value, [(1, 1, 1), (<word_id>, <link_id>, <weight>)])   
+        Example: insert_multiple_into_single_table(Database.Table.word.value, [("wordTuple1",), ("wordTuple2",)]) => <word>
+                 insert_multiple_into_single_table(Database.Table.link.value, [("www.stackoverflow.com/", "German"), ("www.youtube.com", "English")]) => ("<url>", "<language>")])
+                 insert_multiple_into_single_table(Database.Table.wordrelation.value, [(1, 1, 1), (2, 2, 2)]) => (<word_id>, <link_id>, <weight>)   
         '''
+
+        id = None
 
         if(table == self.Table.word.value):
             mySql_insert_query = """INSERT INTO word (word) VALUES (%s)"""
         elif(table == self.Table.link.value):
-            mySql_insert_query = """INSERT INTO link (url) VALUES (%s)"""
+            mySql_insert_query = """INSERT INTO link (url, language) VALUES (%s, %s)"""
         elif(table == self.Table.wordrelation.value):
             mySql_insert_query = """INSERT INTO wordrelation (word_id, link_id, weight) VALUES (%s, %s, %s)"""
 
@@ -90,6 +106,24 @@ class Database:
             self.conn.commit()
         except mariadb.Error as e:
                 print(f"Error: {e}")
+        
+        if(table == self.Table.word.value):
+            valuelist = ', '.join(['"%s"' % w[0] for w in values])
+
+            id = self.get_from_query(f"SELECT id FROM word WHERE word.word IN ({valuelist})")
+        elif(table == self.Table.link.value):
+            valuelist_url = ', '.join(['"%s"' % w[0] for w in values])
+            valuelist_language = ', '.join(['"%s"' % w[1] for w in values])
+
+            id = self.get_from_query(f"SELECT id FROM link WHERE link.url IN ({valuelist_url}) AND link.language IN ({valuelist_language})")
+        elif(table == self.Table.wordrelation.value):
+            valuelist_word_id = ', '.join(['"%s"' % w[0] for w in values])
+            valuelist_link_id = ', '.join(['"%s"' % w[1] for w in values])
+            valuelist_weight = ', '.join(['"%s"' % w[2] for w in values])
+
+            id = self.get_from_query(f"SELECT word_id, link_id FROM wordrelation WHERE word_id IN ({valuelist_word_id}) AND wordrelation.link_id IN ({valuelist_link_id}) AND  wordrelation.weight IN ({valuelist_weight})")
+        
+        return id
     
     def test_my_connection(self):
         '''
